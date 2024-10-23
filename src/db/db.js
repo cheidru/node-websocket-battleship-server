5214f69ebf0dbc7b4d728366b045917fc8355e8f
Igor Checheurov <chei@yandex.ru>
2024-10-23 15:25:59 +0300

    init: start project

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+export class PlayerDB {
+    static players = new Map();
+    constructor () {
+        super();        
+    }
+
+    setPlayer(params) {
+        
+    }
+
+    getPlayer (params) {        
+    }
+
+    deletePlayer (params) {        
+    }
+}
+
+export class roomDB {
+
+}
