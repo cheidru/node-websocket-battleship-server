58674a968438d3245b32bc32e00be0c8eea5d4f4
Igor Checheurov <chei@yandex.ru>
2024-10-25 00:32:32 +0300

    feat: get ws server connected

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,8 +1,14 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer } from 'ws';
+
+function onSocketError(err) {
+    console.error(err);
+}
 
 export const httpServer = http.createServer(function (req, res) {
+    console.log('httpServer started');
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
@@ -14,4 +20,28 @@ export const httpServer = http.createServer(function (req, res) {
         res.writeHead(200);
         res.end(data);
     });
+});
+
+
+// Создаём WebSocket сервер и привязываем его к HTTP серверу
+const wss = new WebSocketServer({ server: httpServer });
+
+// Обрабатываем подключения WebSocket
+wss.on('connection', function (ws) {
+    console.log('Client connected via WebSocket');
+    
+    // Обрабатываем входящие сообщения
+    ws.on('message', function (message) {
+        console.log('received: %s', message);
+        // Ответ клиенту
+        ws.send(`Server received: ${message}`);
+    });
+
+    // Обрабатываем закрытие соединения
+    ws.on('close', function () {
+        console.log('Client disconnected');
+    });
+
+    // Обрабатываем ошибки
+    ws.on('error', onSocketError);
 });
